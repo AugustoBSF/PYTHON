b9ab4eb9850dbdae8f661625d3576f0c59792c1f
AugustoBSF <143547534+AugustoBSF@users.noreply.github.com>
2024-10-24 14:51:04 -0300

    Add files via upload

---
+++ Estrutura de repetição/exemplos.py
@@ -0,0 +1,63 @@
+"""
+x=0
+while x <= 10:
+    print (x)
+    x = x + 1
+    """
+"""
+x = 1
+while True:   
+    print (x)
+  """ 
+
+#tabuada
+
+n = int (input("Digite um número: "))
+x = 1
+while x <= 10:
+    print (n*x)
+    x = x+1 
+
+    #interroper while
+"""
+    
+x = 1 
+while x <= 10:
+    if x == 5:
+        break
+    print (x)
+    x = x + 1    
+""" 
+
+#Estrutura de repetição For
+"""  
+for i  in range (1,6):
+    print(i)
+"""
+"""
+for char in "Augusto":
+    print (char)
+    """
+#Exemplo de for em uma lista de (vetor)
+"""
+lista = [1,2,3,4,5]
+for i in lista:
+    print(i)
+ """
+"""
+computador = ['processador','memoria', 'SSD']
+for i in computador:
+    print (i)
+    """
+
+
+
+"""
+x = 0 
+while True:
+    num = int (input ("Digite um número:"))
+    if  num == 0:
+         break
+    x = x + num
+print(x)        
+"""
